b19b4354eba21743d5d2f8e08104f354e4e6c12d
Mduduzi Sibisi <mdu.sibisi@zoho.com>
2025-08-25 05:27:01 +0200

    Initial Commit
    The first initialization and commit for this project

---
+++ src/main/java/com/example/proveprefilldemo/ProveprefilldemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.proveprefilldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProveprefilldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProveprefilldemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proveprefilldemo/ProveprefilldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proveprefilldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProveprefilldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
